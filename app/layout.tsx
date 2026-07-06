288eb3e91a6c6eacf0ff6fb4b745fe3a3ec531df
Ali Ashraf <aaaaaa30101975@gmail.com>
2026-07-06 10:02:57 +0300

    feat: add Google Tag Manager integration with noscript fallback for improved tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,12 @@ const directionScript = `(function(){
   document.documentElement.lang=locale;
 })();`;
 
+const tagManagerScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TTNNR2N9');`
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -51,9 +57,18 @@ export default function RootLayout({
     >
       <head>
         <script dangerouslySetInnerHTML={{ __html: directionScript }} />
+        <script dangerouslySetInnerHTML={{ __html: tagManagerScript }} />
       </head>
       <body className="min-h-full flex flex-col font-sans">
         <Providers>{children}</Providers>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TTNNR2N9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
       </body>
     </html>
   );
